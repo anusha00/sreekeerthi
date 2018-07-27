ac22e629eb9f7aa3710070364d5fb0e7b44570bf
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 14:36:22 +0530

    Create exponent.java

---
+++ exponent.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int base=2, exponent=3;
+	    long result= 1;
+	    while(exponent !=0)
+	    {
+	    result *=base;
+	    --exponent;
+	}
+	System.out.println("answer= "+result);
+	    
+	}
+}
