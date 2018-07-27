3fcb959d6193da69e87e7313101470bef70509da
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 10:52:14 +0530

    Rename vowel.java to vowelc.java

---
+++ vowelc.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch ='a';
+		if(ch =='a'|| ch == 'i' || ch == 'e' || ch == 'o' || ch == 'u')
+		System.out.print(ch + "is vowel");
+		else
+		System.out.print(ch + "is consonent");
+		
+		
+	}
+}
