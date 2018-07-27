c30d8d93c98cd3aed68717badae6b1d0d07014e4
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 10:29:46 +0530

    Create eo.java

---
+++ eo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		int n=5;
+		if(n%2==0)
+		System.out.print("Even");
+		else
+		System.out.print("Odd");
+		
+	}
+}
