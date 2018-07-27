933405643a510f5c931a9ac2bb2e4197db4a491f
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 15:25:03 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -7,7 +7,7 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int num = 42;
+		int num = 44;
         boolean flag = false;
         for(int i = 2; i <= num/2; ++i)
         {
