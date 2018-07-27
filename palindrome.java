1919aa0950ba705ed9c8f87b02e24325cfd538a0
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 14:55:11 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=121,reversedinteger=0,remainder,originalinteger;
+		System.out.println("enter integer:");
+		originalinteger=n;
+		while(n!=0)
+		{
+			remainder=n%10;
+			reversedinteger=reversedinteger*10+remainder;
+			n/=10;
+			
+		}
+		 if (originalinteger==reversedinteger)
+		System.out.println("yes");
+		else
+		System.out.print("no");
+	}
+}
