d751de8f887c1ecfa8bd33e8e145125ae19dca65
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 11:20:53 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+ class Alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  char c= '&';
+	  if( (c>='a' && c<='z') || (c>='A' && c<='Z') )
+	  System.out.print(c +"Alphabet");
+	  else
+	  System.out.print(c +"No");
+	
+		
+		
+	}
+}
