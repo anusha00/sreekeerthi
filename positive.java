84403c24df8a3e8a9ec239c5b2bd851c5d0980de
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 10:10:41 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=3;
+		if(n%2==0)
+		System.out.print("negitive");
+		else
+		System.out.print("positive");
+ 
+	}
+}
