8040f7affe1d038e76f9b371d43ae29cdb2920c0
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 12:02:19 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=2016;
+		if(n%4==0)
+		{
+		System.out.print("Yes");
+		}
+		else
+		System.out.println("No");
+	}
+}
