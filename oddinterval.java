24c83ac2eddd67e97ee182730aa041b2d9720840
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-27 15:43:47 +0530

    Update oddinterval.java

--- oddinterval.java
+++ oddinterval.java
@@ -6,8 +6,8 @@ import java.io.*;
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
-	{
-		System.out.println("The Odd Numbers are:");
+	
+		
 		for (int i=2; i <= 6; i++) 
 		{
 			if (i % 2!= 0) 
