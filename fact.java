2a78325c3d3d4a182ec6ba71f45dba1fdf26dd13
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-30 20:32:54 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num = 3;
+		int factorial=1;
+                      
+		for(int i = 1; i <= num; ++i)
+		{
+            	factorial = factorial * i;
+           
+		 }
+		 System.out.printf(factorial+ " ");
+            }	
+}
