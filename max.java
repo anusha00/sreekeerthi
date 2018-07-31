d5ff51e1c561ba1f1f45a46bdaf998adabe99d05
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-31 14:24:33 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+		static int arr[] = {-1 , -2 , -3 };
+	
+	static int largest()
+	{
+		int i;
+		
+	
+		int max = arr[0];
+		for (i = 1; i < arr.length; i++)
+			if (arr[i] > max)
+				max = arr[i];
+	
+		return max;
+	}
+		public static void main(String[] args) 
+	{
+		System.out.println(" "  + largest());
+	}
+}
+
