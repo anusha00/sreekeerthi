2640ffdb579fd3abaf264fcb72163682f980ccb1
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-07-31 14:46:12 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+		static int arr[] = {1 , 2 , 3 };
+	
+	static int smallest()
+	{
+		int i;
+		
+	
+		int min = arr[0];
+		for (i = 1; i < arr.length; i++)
+			if (arr[i] < min)
+				min = arr[i];
+	
+		return min;
+	}
+		public static void main(String[] args) 
+	{
+		System.out.println(" "  + smallest());
+	}
+}
+
