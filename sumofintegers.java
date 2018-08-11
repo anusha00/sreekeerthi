800f9716a9069c9e7fdfe70e7cd4d5e07f6f9021
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-11 10:20:06 +0530

    Create sumofintegers.java

---
+++ sumofintegers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class SumofGivenIntegers 
+{
+	public static void main(String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println(" ");
+		int N=in.nextInt();
+		int K=in.nextInt();
+		int sum=0;
+		int a[]=new int[N];
+		for (int i = 0; i < a.length; i++)
+		{
+			a[i]=in.nextInt();
+		}
+		for(int i=0;i<K;i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(" ");
+		System.out.println(sum);		
+	}
+}
