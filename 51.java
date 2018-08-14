2b919219f5c08660d3adb20e1e7ece67fea37172
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-14 11:51:15 +0530

    Create 51.java

---
+++ 51.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class sameorder
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int reversenum=0;
+		int count=0;
+		 while( n!= 0 )
+      {
+             reversenum = reversenum * 10;
+             reversenum = reversenum + n%10;
+             n= n/10;
+             count++;
+      }
+
+		while(reversenum>0)
+		{
+			int digit=reversenum%10;
+			System.out.print(digit);
+		            if(count!=(count-1))
+			System.out.print(" ");
+			reversenum=reversenum/10;
+		}
+	}
