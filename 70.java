a4dfaf701f4e1748e38ff1551572eef056630e57
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-14 13:53:41 +0530

    Create 70.java

---
+++ 70.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+ class Ideone
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		System.out.println(2*n);
+		s.close();
+	}
+}
