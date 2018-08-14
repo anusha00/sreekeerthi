9405f001d0d359dc9fb642f49a1e4ceec3a44cc5
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-14 16:15:24 +0530

    Create 68.java

---
+++ 68.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class multiple
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n%7==0)
+		{
+			System.out.println("yes");
+			
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
