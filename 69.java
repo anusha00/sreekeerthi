b555a9eb0248871bf92116f32309e572f34e0b06
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-14 13:50:11 +0530

    Create 69.java

---
+++ 69.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Ideone
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        int s=Math.abs(n-m);
+            if(s%2==0)
+            {
+                System.out.print("even");
+            }
+            else
+            System.out.print("odd");
+}
+}
