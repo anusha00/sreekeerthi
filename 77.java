63fbbf8679535664761e3193bf33959e25455a2d
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-16 10:44:48 +0530

    Create 77.java

---
+++ 77.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ class sdr
+ {
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++) 
+		{
+			if(n%i==0)
+			{
+				System.out.print(i+" ");
+			}
+		
+	}
+  }
+}	            
