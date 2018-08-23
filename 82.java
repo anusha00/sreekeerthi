fad1e98484b7a0b988e7a8b4bce51e32cd1042f9
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-23 09:52:54 +0530

    Update 82.java

--- 82.java
+++ 82.java
@@ -3,21 +3,18 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class lenbre
+class Ideone1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		        float a,d;
-        Scanner s = new Scanner(System.in);
-        a= s.nextFloat();
-        d = s.nextFloat();
-                float res=a*d;
-	        System.out.format("%.5f", res);
-
-                
-
-        
-
+		Scanner sc=new Scanner(System.in);
+		double a,b,c;
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		c=a*b;
+		System.out.format("%.5f",c);
 	}
 }
+
+
