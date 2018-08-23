84ad04c63c3fff62c7d38ef10eb487c353c33a68
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-23 10:13:36 +0530

    Create 88.java

---
+++ 88.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class lcm
+{ 
+  static int lcm(int x, int y)
+    {
+        int a;
+        a = (x > y) ? x : y; // a is greater number
+        while(true)
+        {
+            if(a % x == 0 && a % y == 0)
+                return a;
+            ++a;
+        }	
+    }
+ 
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+ 
+       
+        System.out.println(lcm(x, y));
+	}
+}
+Not connected. Connecting in 6s…
+Try now
