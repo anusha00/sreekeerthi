6905a5660c6d2eb299a34937eb190123fd3bf55f
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-23 10:49:12 +0530

    Create 93.java

---
+++ 93.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class temp
+ {
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	 int temp=in.nextInt();
+           int  kelvin=273;
+	 kelvin=kelvin+temp;
+	 System.out.println(kelvin);
+	}
+}
