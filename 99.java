cd549d84e4a8a629021e936a87c94e2e93386bac
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-23 12:29:15 +0530

    Create 99.java

---
+++ 99.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class mod
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+System.out.println((a*b)%c);
+	}
+
+}
