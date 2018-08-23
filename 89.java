9eefc6be5a8e09f073a7c18d84bd9c6a6b0cdec3
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-23 10:31:43 +0530

    Create 89.java

---
+++ 89.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+ class lexographical {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+       String n=s.next();
+       char ch[]=n.toCharArray();
+       Arrays.sort(ch);
+       String m="";
+       for(int i=0;i<ch.length;i++)
+       {
+    	 m+=ch[i];  
+       }
+       System.out.println(m);
+	}
+
+}
