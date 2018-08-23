25266ec4ec36d53d7d5332131159bf4cfdf75b84
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-23 11:00:01 +0530

    Create 95.java

---
+++ 95.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class floor
+ {
+
+	public static void main(String[] args)
+	{
+              Scanner in=new Scanner(System.in);
+              int p=in.nextInt();
+              int t=in.nextInt();
+              int r=in.nextInt();
+              int si=(p*t*r)/100;
+              System.out.println(si);
+	}
+
+}
