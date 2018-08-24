ae6ba6b89b865bbb5a27ef5356b126848380373a
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-24 09:58:57 +0530

    Create 105.java

---
+++ 105.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class join
+{
+
+	public static void main(String[] args) 
+	{
+Scanner in=new Scanner(System.in);
+String str=in.nextLine();
+String s=str.replaceAll(" ","");
+System.out.println(s);
+	}
+
+}
