53b3adf5f607e1729f48afffd31defe1cc8e6fb5
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-24 12:43:19 +0530

    Rename 101.java to 101s.java

---
+++ 101s.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Set107 
+{
+	public static void main(String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=in.nextInt();
+		int s=(a*b)/c;
+		System.out.println(s);
+	}
+}
