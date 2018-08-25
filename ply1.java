a55245803cbaed05a0a15ff7d6d1e4ea5d00be7a
tanneerukeerthi <41734111+tanneerukeerthi@users.noreply.github.com>
2018-08-25 17:43:25 +0530

    Update ply1.java

--- ply1.java
+++ ply1.java
@@ -3,14 +3,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class reverse
+class reversestring
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		String s=sc.next();
-		String s1= " ";
+		String s1= "";
 		for(int i=s.length()-1;i>=0;i--)
 		{
 			s1=s1+s.charAt(i);
